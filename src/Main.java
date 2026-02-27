5639750aa262bb1a32d41bf4ad1d5095cc3d8c16
lordiring90 <lordiring2000@gmail.com>
2026-02-27 17:26:36 -0500

    Cambio de poner el objeto/personaje a ponerlo con lista

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,64 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
 
-        PersonajeEXE p1 = new PersonajeEXE("Pedrito", 1, 0, 100, true);
+        //PersonajeEXE p1 = new PersonajeEXE("Pedrito", 1, 0, 100, true);
 
+        List<PersonajeEXE> lisObjeto = new ArrayList<>();
+
+        int op;
+        System.out.println("Seleccione una opcion para crear Objeto");
+        op = teclado.nextInt();
+
+        do {
+            System.out.println("1. Crear Personaje");
+            System.out.println("2. Mostrar Personaje");
+            System.out.println("3. Salir");
+
+            switch (op){
+                case 1:
+                    System.out.println("Ingrese el nombre del personaje");
+                    String nombre = teclado.next();
+
+                    System.out.println("Ingrese el nivel del personaje");
+                    int nivel = teclado.nextInt();
+
+                    System.out.println("Ingrese la experiencia del personaje");
+                    int experiencia = teclado.nextInt();
+
+                    System.out.println("Ingrese la vida del personaje");
+                    int vida = teclado.nextInt();
+
+                    boolean vivo = true;
+
+
+                    PersonajeEXE p1 = new PersonajeEXE(nombre, nivel, experiencia, vida, vivo);
+                    lisObjeto.add(p1);
+                    break;
+
+                case 2:
+                    for (PersonajeEXE mostrar: lisObjeto){
+                        System.out.println(mostrar);
+                    }
+                    break;
+
+
+
+                case 3:
+                    System.out.println("Salir chao bye");
+
+                default:
+                    System.out.println("Elige dentro de los parametros");
+
+            }
+
+
+        } while (op != 3);
+
+        //Menu de acciones aqui no tocar
         int opcion;
         do {System.out.println("\n================== \n menu de acciones");
             System.out.println("1. mostrar estado");
